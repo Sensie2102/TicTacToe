ba380f0ec412c38edc271c6b89d3c35fd650049c
Sensie21 <mafaaz.sabir@gmail.com>
2023-09-22 09:50:42 +0530

    Game Ready

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,17 +1,3 @@
-let p1_turn = true
-
-$(".grid div").click(function(){
-    let clicked_ele = this
-    if(p1_turn){
-        $(clicked_ele).text("X")
-        p1_turn = false
-    }else{
-        $(clicked_ele).text("O")
-        p1_turn = true
-    }
-    
-})
-
-// $("#reset").click(function (){
-//     location.reload()
-// })
+const player_X = "X"
+const player_O = "O"
+let turn = player_X
